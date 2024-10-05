01a9ae3ffcdbcefb23a8b9e809ad7d2e523e0d71
Mr.D <danil36rusdanil@mial.ru>
2024-10-05 12:44:08 +0300

    кое-что изменил

--- Hospital-microservice/src/main/java/com/example/Hospital_microservice/restclient/RestTemplateConfiguration.java
+++ Hospital-microservice/src/main/java/com/example/Hospital_microservice/restclient/RestTemplateConfiguration.java
@@ -3,19 +3,14 @@ package com.example.Hospital_microservice.restclient;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestClientConfiguration {
+public class RestTemplateConfiguration {
 
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
 
-    @Bean
-    public RestClient restClient(){
-        return RestClient.create();
-    }
 }
