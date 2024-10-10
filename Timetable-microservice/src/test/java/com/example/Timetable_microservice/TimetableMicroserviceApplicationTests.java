62ea4547c4cb2344c27571857e10690c40c0c64a
Mr.D <danil36rusdanil@mial.ru>
2024-10-10 22:24:17 +0300

    добавил недостающие файлы

---
+++ Timetable-microservice/src/test/java/com/example/Timetable_microservice/TimetableMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Timetable_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
